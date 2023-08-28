96e867a603f9d1fd2cc564f55f77024e96720073
ShinMinOh <luciano97@naver.com>
2023-08-28 17:19:48 +0900

    OrderAPI using DTO

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,16 +1,22 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne)에서 성능 최적화를 어떻게 할것인지에 대한것.
+ * 주문+배송정보+회원 조회하는 API
  * Order
  * Order -> Member : Order와 Member는 ManyToOne
  * Order -> Delivery : Order 와 Delivery는 OneToOne
@@ -21,6 +27,10 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
+    /**
+     * V1. 엔티티 직접 노출(fetch join 사용X)
+     * 양방향 관계 문제 발생 -> @JsonIgnore
+     * */
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1(){
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -33,5 +43,37 @@ public class OrderSimpleApiController {
         }
 
         return all;
+    }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * 단점 : 지연로딩으로 쿼리 N번 호출
+     * */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDtoV2> ordersV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch()); //이대로 반환하면 안됨. SimpleOrderDto타입으로 바꿔야함.
+        List<SimpleOrderDtoV2> result = orders.stream()
+                .map(o -> new SimpleOrderDtoV2(o))
+                .collect(Collectors.toList());
+
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDtoV2 {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address; //배송지정보
+
+        public SimpleOrderDtoV2(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화 : 영속성컨텍스트가 memberid를 가지고 찾고 없으면 db에 쿼리날림.
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
     }
 }
